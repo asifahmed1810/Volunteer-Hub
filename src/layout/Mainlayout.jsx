6e8e9bd0f65c7157b1311c3333c414b3b8ac015e
asifahmed1810 <asifahmed18oct@gmail.com>
2024-12-22 07:42:10 +0600

    Added Mainlayout

---
+++ src/layout/Mainlayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Mainlayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Mainlayout;
---
+++ src/router/router.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Mainlayout from '../layout/Mainlayout';
+import Home from '../pages/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout></Mainlayout>,
+    errorElement: <h2>Route Not Found</h2>,
+    children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
+    ]
+  },
+]);
+
+export default router;
