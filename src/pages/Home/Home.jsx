bdc09d2a8a813247f8c10f2af7352f8764fafcb2
asifahmed1810 <asifahmed18oct@gmail.com>
2024-12-23 21:43:37 +0600

    added banner to the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../../shared/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <h2>THis is home</h2>
+            <Banner></Banner>
         </div>
     );
 };
